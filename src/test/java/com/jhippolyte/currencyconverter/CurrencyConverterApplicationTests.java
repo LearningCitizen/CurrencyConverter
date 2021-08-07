69476ae2190225b83fe411ff7c09b5d8067e29e8
John-son Hippolyte <contact@jhippolyte.com>
2021-08-07 20:58:02 +0200

    BREAKING CHANGE: init of the project

---
+++ src/test/java/com/jhippolyte/currencyconverter/CurrencyConverterApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jhippolyte.currencyconverter;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CurrencyConverterApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
