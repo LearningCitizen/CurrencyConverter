46f9eeebea5109ef442d9ba5401020ad78690dab
John-son Hippolyte <contact@jhippolyte.com>
2021-08-09 16:28:09 +0200

    create CurrencyRepository

---
+++ src/main/java/com/jhippolyte/currencyconverter/repository/CurrencyRepository.java
@@ -0,0 +1,33 @@
+package com.jhippolyte.currencyconverter.repository;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
+
+import com.jhippolyte.currencyconverter.model.Currency;
+
+public class CurrencyRepository {
+
+    private static List<Currency> currencies ; 
+    
+    public static void init() {
+    	currencies = new ArrayList<Currency>();
+    	currencies.add(new Currency("US Dollar", "USD", 1.0));
+    	currencies.add(new Currency("Euro", "EUR", 0.85073));
+    	currencies.add(new Currency("British Pound", "GBP", 0.72114));
+    	currencies.add(new Currency("Indian Rupee", "INR", 74.292));
+    	currencies.add(new Currency("Australian Dollar", "AUD", 1.3598));
+    	currencies.add(new Currency("Canadian Dollar", "CAD", 1.2561));
+    	currencies.add(new Currency("Singapore Dollar", "SGD", 1.3557));
+    	currencies.add(new Currency("Swiss Franc", "CHF", 0.91820));
+    }
+    
+    public static List<Currency> findAllCurrencies() {
+    	return currencies;
+    }
+    
+    public Optional<Currency> findCurrencyByTrigram (String trigram) {
+    	return Optional.of(currencies.stream().filter(cur -> cur.getTrigram().equals(trigram))
+    			.findFirst().orElse(null));
+    }
+}
