055d986d8411f1efd2177525b10765c05fa260d0
John-son Hippolyte <contact@jhippolyte.com>
2021-08-09 20:58:10 +0200

    add Loggin in CurrencyService

--- src/main/java/com/jhippolyte/currencyconverter/service/CurrencyServiceImpl.java
+++ src/main/java/com/jhippolyte/currencyconverter/service/CurrencyServiceImpl.java
@@ -4,6 +4,8 @@ import java.util.List;
 import java.util.Optional;
 import java.util.stream.Collectors;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 
 import com.jhippolyte.currencyconverter.exception.CurrencyException;
@@ -14,15 +16,19 @@ public class CurrencyServiceImpl implements CurrencyService {
 
 	@Autowired
 	CurrencyRepository currencyRepository;
+	
+	private static Logger logger = LoggerFactory.getLogger(CurrencyServiceImpl.class);
 
 	@Override
 	public List<String> getCurrenciesList() {
+		logger.info("Getting list of currencies");
 		return currencyRepository.findAllCurrencies()
 				.stream().map(cur -> cur.toString()).toList();
 	}
 
 	@Override
 	public double convertCurrency(String currencySource, String currencyTarget, double amount) throws CurrencyException {
+		logger.info("trying getting source currency and target currency");
 		Optional<Currency> currSourceOpt = currencyRepository.findCurrencyByTrigram(currencySource);
 		Optional<Currency> currTargetOpt = currencyRepository.findCurrencyByTrigram(currencyTarget);
 		if (currSourceOpt.isEmpty())
@@ -31,7 +37,7 @@ public class CurrencyServiceImpl implements CurrencyService {
 			throw new CurrencyException("The Currency "+currencyTarget+" does not exist in our database");
 		Currency currSource = currSourceOpt.get();
 		Currency currTarget = currTargetOpt.get();
-
+		logger.info("converting in the target currency");
 		return (currSource.getExchangeRate()*amount)/currTarget.getExchangeRate();
 	}
 	
