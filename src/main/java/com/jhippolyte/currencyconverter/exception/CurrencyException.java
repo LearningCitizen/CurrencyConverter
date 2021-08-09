072736a3bd4697cf9eea552d18fe67465f25b7c5
John-son Hippolyte <contact@jhippolyte.com>
2021-08-09 17:10:32 +0200

    create CurrencyException

---
+++ src/main/java/com/jhippolyte/currencyconverter/exception/CurrencyException.java
@@ -0,0 +1,9 @@
+package com.jhippolyte.currencyconverter.exception;
+
+public class CurrencyException extends Exception {
+
+	public CurrencyException (String errorMessage)
+	{
+		super(errorMessage);
+	}
+}
