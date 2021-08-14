f266252bb97b5698068cdc85ba1a131f30da2219
John-son Hippolyte <contact@jhippolyte.com>
2021-08-14 18:54:48 +0200

    implements header

--- curr-conv-front/src/app/components/header/header.component.ts
+++ curr-conv-front/src/app/components/header/header.component.ts
@@ -7,6 +7,8 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HeaderComponent implements OnInit {
 
+  title: string = "Currency converter";
+  
   constructor() { }
 
   ngOnInit(): void {
