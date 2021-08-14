6173a4dc403fb73033a3a28ced0b9fca10b1ecb7
John-son Hippolyte <contact@jhippolyte.com>
2021-08-14 18:42:59 +0200

    generate select component

--- curr-conv-front/src/app/app.module.ts
+++ curr-conv-front/src/app/app.module.ts
@@ -4,12 +4,14 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
 import { ButtonComponent } from './components/button/button.component';
 import { HeaderComponent } from './components/header/header.component';
+import { SelectComponent } from './components/select/select.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     ButtonComponent,
-    HeaderComponent
+    HeaderComponent,
+    SelectComponent
   ],
   imports: [
     BrowserModule
