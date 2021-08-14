71569923d54b4f5d1c5bb7b178bd77d8ba787b57
John-son Hippolyte <contact@jhippolyte.com>
2021-08-14 23:02:57 +0200

    init implementationof select

--- curr-conv-front/src/app/components/select/select.component.ts
+++ curr-conv-front/src/app/components/select/select.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
 
 @Component({
   selector: 'app-select',
@@ -7,9 +7,15 @@ import { Component, OnInit } from '@angular/core';
 })
 export class SelectComponent implements OnInit {
 
+  @Input() name: string = "select";
+
   constructor() { }
 
   ngOnInit(): void {
+  }
+
+  size(){
+    return 0;
   }
 
 }
