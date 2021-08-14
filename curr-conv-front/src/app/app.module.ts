1c94dd1fdb826d48a6dc1e6a756915dfdc0cac82
John-son Hippolyte <contact@jhippolyte.com>
2021-08-14 18:41:53 +0200

    generate header

--- curr-conv-front/src/app/app.module.ts
+++ curr-conv-front/src/app/app.module.ts
@@ -3,11 +3,13 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 import { ButtonComponent } from './components/button/button.component';
+import { HeaderComponent } from './components/header/header.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    ButtonComponent
+    ButtonComponent,
+    HeaderComponent
   ],
   imports: [
     BrowserModule
