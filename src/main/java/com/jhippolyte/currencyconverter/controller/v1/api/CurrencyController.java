2a5eeb6c52b6fb3b647f7ac8e6a4623a1c8a73e2
John-son Hippolyte <contact@jhippolyte.com>
2021-08-16 00:46:12 +0200

    allow cors for Angular

--- src/main/java/com/jhippolyte/currencyconverter/controller/v1/api/CurrencyController.java
+++ src/main/java/com/jhippolyte/currencyconverter/controller/v1/api/CurrencyController.java
@@ -7,6 +7,7 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -19,6 +20,7 @@ import com.jhippolyte.currencyconverter.service.CurrencyService;
 @Api(value = "currency-converter-api")
 @RestController
 @RequestMapping("/api/v1/currencies")
+@CrossOrigin(origins = "http://localhost:4200")
 public class CurrencyController {
 
     private Logger logger = LoggerFactory.getLogger(CurrencyController.class);
