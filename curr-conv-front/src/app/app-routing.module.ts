92088b5512fcca5620757137b1c29b067175dc9d
John-son Hippolyte <contact@jhippolyte.com>
2021-08-17 15:59:10 +0200

    add currencies component in Route

--- curr-conv-front/src/app/app-routing.module.ts
+++ curr-conv-front/src/app/app-routing.module.ts
@@ -1,10 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { ConversionComponent } from './components/conversion/conversion.component';
+import { CurrenciesComponent } from './components/currencies/currencies.component';
 
 const routes: Routes = [
   { path : '', redirectTo : '/converter', pathMatch : 'full' },
-  { path: 'converter', component: ConversionComponent }
+  { path: 'converter', component: ConversionComponent },
+  { path: 'currencies', component: CurrenciesComponent }
 ]
 
 @NgModule({
